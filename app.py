4084381ab6803af6995b6fa2e16343605392390f
Gözde Işıldak <51263887+gozdeisildak@users.noreply.github.com>
2020-06-16 13:07:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask
+import re
+from flask import Flask, redirect, url_for, render_template, request, session, Response
+from datetime import timedelta
+from flask import flash
+from forms import RegistrationForm, LoginForm
+import gc
+from flask_mysqldb import MySQL
+import yaml
+import os
+import cv2
+import numpy as np
+from camera import VideoCamera, UploadImage
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import date
+import MySQLdb
+
+upld = UploadImage()
 app = Flask(__name__)
+app.secret_key = "hello"
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World gozo!'
+
+@app.route("/aboutUs")
+def aboutUs():
+    return render_template("tutorial-components.html")
 
 if __name__ == '__main__':
     app.run()
