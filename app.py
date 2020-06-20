337b19f992a3d276dd787c246baea66dfed2bffd
Gözde Işıldak <51263887+gozdeisildak@users.noreply.github.com>
2020-06-21 01:39:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,23 @@ def upload():
     flash("test")
     return render_template("upload.html")
 
+@app.route('/video')
+def index():
+    return render_template('video.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == '__main__':
     app.run()
